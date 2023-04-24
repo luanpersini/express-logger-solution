0870399dc4e312b22b798edfc2a1fc161fa5d7ac
Luan Persini <lpersini@pmcgroupone.com>
2023-04-24 20:29:41 -0300

    refactor: log server error status

--- src/middlewares/TraceIdMiddleware.js
+++ src/middlewares/TraceIdMiddleware.js
@@ -1,6 +1,6 @@
-const uuid = require('uuid')
+const uuid = require('uuid');
 
 module.exports = (req, res, next) => {
   req.traceId = req.headers.traceId ? req.headers.traceId.toString() : uuid.v4();
   next();
-}
+};
